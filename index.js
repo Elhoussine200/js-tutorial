37903d5983d31661e5d8ab55c7ff822f2800a9a5
Elhoussine200 <160263585+Elhoussine200@users.noreply.github.com>
2024-05-04 19:18:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // ask the user for the first value
+console.clear()
 let noteNational = prompt("ch7al jebti f lwatani ")
 let noteRegional = prompt("ch7al jebti f Regional")
 let anneeScolaire = prompt("ch7al jebti f mora9aba")
-let total = Number(noteNational)+ Number(noteRegional)+ Number(anneeScolaire)
+let total = Number(noteNational)*2+ Number(noteRegional)+ Number(anneeScolaire)
 total = total/4
-alert (total)
+if (total>=10){
+    console.log ("Mabroooook jebty",total)
+}
+
+else if (total<10 && total>7 ){
+    console.log (" 3ndk ratrappage jbti ",total,"zgueltih",10-total)
+}
+
+else {
+  console.log ("7lawet lbac 3years",total)
+  
+}
+
+
+ 
