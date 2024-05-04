5ecd7f7b72030a92d5a57ab96e1f286a6ad5035a
Elhoussine200 <160263585+Elhoussine200@users.noreply.github.com>
2024-05-04 18:55:32 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// ask the user for the first value
+let noteNational = prompt("ch7al jebti f lwatani ")
+let noteRegional = prompt("ch7al jebti f Regional")
+let anneeScolaire = prompt("ch7al jebti f mora9aba")
+let total = Number(noteNational)+ Number(noteRegional)+ Number(anneeScolaire)
+total = total/4
+alert (total)
